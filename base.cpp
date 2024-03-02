672297bf69d61fce766e22758dbeb13c4ea70454
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2024-03-02 11:20:06 -0300

    update base

---
+++ base.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+
+#define rep(i, begin, end) for(int i = begin; i < end; i++)
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+#define sq(x) (x)*(x)
+
+const int N = 112345;
+const int inf = 1e9+7;
+
+void solvetask()
+{
+    int n;
+    cin >> n;
+}
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solvetask();
+        // cout << (solvetask() ? "YES" : "NO") << '\n';
+}
