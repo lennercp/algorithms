9ade8f5cb77663aca1123211030fb5c15142dbfa
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2025-07-20 14:04:31 -0300

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -1,12 +1,20 @@
+#ifdef _DEBUG
+    #include "debug.hpp"
+#else
+    #define dbg(head, ...)  ((void)0)
+#endif
+
 #include "bits/stdc++.h"
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 
 #define rep(i, begin, end) for(int i = begin; i < end; i++)
 #define all(x) x.begin(),x.end()
-#define allr(x) x.rbegin(),x.rend()
+#define allr(x) x.rbegin(), x.rend()
 #define sz(x) (int)x.size()
 #define sq(x) (x)*(x)
+#define endl '\n'
 
 const int N = 112345;
 const int inf = 1e9+7;
